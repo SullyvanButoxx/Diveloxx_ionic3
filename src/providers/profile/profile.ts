fd3353eb4879d0c64e6060107abf300c4a0f8cb5
SullyvanButoxx <butoxxdev@gmail.com>
2018-09-21 16:09:01 +0200

    Add create function

--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { User } from '../../models/user';
 
 @Injectable()
 export class ProfileProvider {
 
-  constructor() {
+  constructor(private afStore: AngularFirestore) {
+  }
+
+  create(user: User) {
+    return this.afStore.doc<any>(`profile/${user.id}`).set(user.profile)
   }
 
 }
