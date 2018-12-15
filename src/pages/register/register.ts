5cc50b36448fd9e3a936c3e9c09b40368a3424d2
SullyvanButoxx <butoxxdev@gmail.com>
2018-12-15 17:06:48 +0100

    Add required validation for register

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -3,7 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
 import { UserProvider } from '../../providers/user/user';
 import { ProfileProvider } from '../../providers/profile/profile';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Profile } from '../../models/profile';
 
 @Component({
@@ -17,23 +17,30 @@ export class RegisterPage {
   
   constructor(public navCtrl: NavController, public navParams: NavParams, private userProvider: UserProvider, private profileProvider: ProfileProvider, private formBuilder: FormBuilder) {
     this.registerForm = this.formBuilder.group({
-      email: [''],
-      password: [''],
-      username: [''],
-      firstname: [''],
-      lastname: ['']
+      email: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+      username: ['', Validators.compose([Validators.required])],
+      firstname: ['', Validators.compose([Validators.required])],
+      lastname: ['', Validators.compose([Validators.required])]
     })
   }
 
   async register() {
-    try {
-      this.user = new User("",this.registerForm.controls.email.value, this.registerForm.controls.password.value, new Profile(this.registerForm.controls.username.value,this.registerForm.controls.firstname.value,this.registerForm.controls.lastname.value))
-      let userAuth = await this.userProvider.create(this.user)
-      this.user.id = userAuth.user.uid
-      await this.profileProvider.create(this.user)
-      console.log(this.user)
-    } catch (error) {
-      console.error(error)
+    // Check input validation
+    if (this.registerForm.valid) {
+      // Validation success
+      try {
+        this.user = new User("",this.registerForm.controls.email.value, this.registerForm.controls.password.value, new Profile(this.registerForm.controls.username.value,this.registerForm.controls.firstname.value,this.registerForm.controls.lastname.value))
+        let userAuth = await this.userProvider.create(this.user)
+        this.user.id = userAuth.user.uid
+        await this.profileProvider.create(this.user)
+        console.log(this.user)
+      } catch (error) {
+        console.error(error)
+      }
+    } else {
+      // Validation fail
+      console.log('All inputs must be filled')
     }
   }
 }
